28b6f43849f290d2e2d8f521b6e4f7b0f0975bd2
armandopato <armauv@gmail.com>
2021-02-01 11:26:02 -0600

    Implement CSV generation and custom filenames

--- main.c
+++ main.c
@@ -18,6 +18,11 @@
 
 #define QUALITY 100
 
+
+char *get_image_name(char *image_path);
+
+char *generate_new_name(char *original, char *suffix);
+
 long *generate_histogram(unsigned char *image, long size, int number_of_channels);
 
 unsigned char *generate_empty_unsigned_char_array(long size);
@@ -34,6 +39,7 @@ long *get_equalized_cumulative_distribution(long *cumulative_distribution, long 
 
 unsigned char *generate_image(unsigned char *original_image, int number_of_channels, long *equalized_cumulative_distribution, long size);
 
+void generate_csv(long *original_histogram, long *equalized_histogram, char* suffix);
 
 int main(int argc, char *argv[])
 {
@@ -65,12 +71,48 @@ int main(int argc, char *argv[])
     long *equalized_cumulative_distribution = get_equalized_cumulative_distribution(cumulative_distribution, cumulative_distribution_minimum, size);
     unsigned char *new_image = generate_image(image, number_of_channels, equalized_cumulative_distribution, size);
     long *new_histogram = generate_histogram(new_image, size, number_of_channels);
-    // create path with suffix
-    // Generate new image
-    const char *image_name = "./out/caca.jpg";
-    stbi_write_jpg("caca.jpg", width, height, number_of_channels, new_image, QUALITY);
-    //free used
+
+    char *original_name = get_image_name(image_path);
+    char *new_name = generate_new_name(original_name, "_eq_sequential.jpg");
+    stbi_write_jpg(new_name, width, height, number_of_channels, new_image, QUALITY);
+    generate_csv(histogram, new_histogram, "sequential");
+
+    stbi_image_free(image);
+    stbi_image_free(new_image);
+    free(histogram);
+    free(new_histogram);
+    free(cumulative_distribution);
+    free(equalized_cumulative_distribution);
+    free(original_name);
+    free(new_name);
     return 0;
+}
+
+void generate_csv(long *original_histogram, long *equalized_histogram, char* suffix)
+{
+    char *filename = generate_new_name("out/histogram_", suffix);
+    filename = generate_new_name(filename, ".csv");
+    FILE *csv = fopen(filename, "w+");
+
+    for (int i = 0; i < MAX_COLOR_VALUE; i++)
+        fprintf(csv, "%d,%ld,%ld\n", i, original_histogram[i], equalized_histogram[i]);
+
+    fclose(csv);
+}
+
+char *get_image_name(char *image_path)
+{
+    char *image_name = strrchr(image_path, '/');
+    if (image_name == NULL) image_name = image_path;
+    else image_name++;
+    return strtok(image_name, ".");
+}
+
+char *generate_new_name(char *original, char *suffix)
+{
+    char *new_image_name = malloc(sizeof(char) * 1000);
+    snprintf(new_image_name, sizeof(char) * 1000, "%s%s", original, suffix);
+    return new_image_name;
 }
 
 unsigned char *generate_empty_unsigned_char_array(long size)
@@ -157,7 +199,7 @@ unsigned char *generate_image(unsigned char *original_image, int number_of_chann
 {
     unsigned char *new_image = generate_empty_unsigned_char_array(size * number_of_channels);
 
-    for (int i = 0; i < size * number_of_channels; i += number_of_channels)
+    for (int i = 0; i < size * number_of_channels; i++)
     {
         unsigned char original_value = original_image[i];
         if (i % number_of_channels == 0) new_image[i] = (unsigned char) equalized_cumulative_distribution[original_value];
